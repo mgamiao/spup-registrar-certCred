e4c6a08fb468d4ad0a6d2c4a707955d7b7f3f983
Miguel Gamiao <miguelgamiao@gmail.com>
2021-05-05 13:48:40 +0800

    updated chart data

--- home.php
+++ home.php
@@ -25,7 +25,7 @@ include "header.php" ?>;
 <div class="row">
 
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <!-- <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
       <div class="card-body">
         <div class="row no-gutters align-items-center">
@@ -39,12 +39,12 @@ include "header.php" ?>;
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
  
 
   <!-- Pending Requests Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <!-- <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-warning shadow h-100 py-2">
       <div class="card-body">
         <div class="row no-gutters align-items-center">
@@ -59,7 +59,7 @@ include "header.php" ?>;
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 
 <script src="js/demo/chart-area-demo.js"></script>
